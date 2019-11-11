f459376e4a0deeef931a66035664276321ce38b7
anderara <andc91@gmail.com>
2019-11-11 15:18:40 +0100

    Clean up code reducer 'gameTwo'

--- src/reducers/gameTwo.js
+++ src/reducers/gameTwo.js
@@ -1,9 +1,7 @@
 const reducer = (state = [], action = {}) => {
-    // console.log("this is the state",state, "this is the action",action)
       switch (action.type) {
-  
+
       case "SET_DOG_QUESTION":
-          
         return [
           ...state,
           ...action.payload
