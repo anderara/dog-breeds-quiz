f92eff32065f4daaabe361e4b73ebcefe7f7bbae
anderara <andc91@gmail.com>
2019-11-14 11:14:38 +0100

    Clean up code

--- src/components/GameTwo.js
+++ src/components/GameTwo.js
@@ -1,47 +1,46 @@
 import React, {Component} from 'react'
-import { startGame } from '../actions/api'
-import {wrongAnswer, correctAnswer, isItCorrect} from '../actions/answer'
+import { isItCorrect } from '../actions/answer'
 import { connect } from 'react-redux'
 
 class GameTwo extends Component {
-render() {
-    if(!this.props.randomDogsImages) return "Loading"
-    if(!this.props.actualBreedsDisplayed) return "Loading"
+    render() {
+        if(!this.props.randomDogsImages) return "Loading"
+        if(!this.props.actualBreedsDisplayed) return "Loading"
 
-    const arrayUrls =  this.props.randomDogsImages
-    const arrayBreeds = this.props.actualBreedsDisplayed
-    const displayedPicture = arrayUrls[Math.floor(Math.random()*arrayUrls.length)]
+        const arrayUrls =  this.props.randomDogsImages
+        const arrayBreeds = this.props.actualBreedsDisplayed
+        const displayedPicture = arrayUrls[Math.floor(Math.random()*arrayUrls.length)]
 
-    const Handleclick = (event) =>{
-        const chosenBreed = event.target.value
-        const answer = displayedPicture.search(chosenBreed)
+        const Handleclick = (event) =>{
+            const chosenBreed = event.target.value
+            const answer = displayedPicture.search(chosenBreed)
 
-        this.props.isItCorrect(answer, displayedPicture)
-    }
+            this.props.isItCorrect(answer, displayedPicture)
+        }
 
-   return (
-    <div>
-        <h2>Game #1</h2>
-        <p>Please select the breed of the dog below</p>
-        <img id={displayedPicture} src={displayedPicture} width="330" height="270" alt=""/>
-
+    return (
         <div>
-            <ul>
-                {arrayBreeds.length > 0
-                    ? arrayBreeds.map(breed =>
-                        <button key={breed} value={breed} onClick={Handleclick}>{breed}</button>
-                    )
-                    :'loading...'
-                }
-            </ul>
+            <h2>Game #1</h2>
+            <p>Please select the breed of the dog below</p>
+            <img id={displayedPicture} src={displayedPicture} width="330" height="270" alt=""/>
+
+            <div>
+                <ul>
+                    {arrayBreeds.length > 0
+                        ? arrayBreeds.map(breed =>
+                            <button key={breed} value={breed} onClick={Handleclick}>{breed}</button>
+                        )
+                        :'loading...'
+                    }
+                </ul>
+            </div>
         </div>
-    </div>
-    )
-}
+        )
+    }
 }
 
 const mapStateToProps = (state) => {
 	return {}
 }
 
-export default connect(mapStateToProps, {startGame, wrongAnswer, correctAnswer, isItCorrect})(GameTwo)
+export default connect(mapStateToProps, { isItCorrect })(GameTwo)
