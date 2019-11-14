acfe06052ae31b4c44572e9c7616aeafbfa0d095
anderara <andc91@gmail.com>
2019-11-14 11:04:22 +0100

    Clean up code

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,11 +10,11 @@ export default function(props) {
         <h1>Doggo Game</h1>
     </main>
     <div>
-        <h2> Welcome to "de beste" DOGGO GAME out there! </h2>
-        <h3>Are you ready to play? </h3>
+        <h2> Are you ready to play? </h2>
+        <br/>
         <img id={gifDisplayed} src={gifDisplayed} alt="" width="380" height="300" />
-        <p>Please select <strong>"Breeds List"</strong> to explore all the breeds </p>
-        <p>Please select <strong>"Game One"</strong> or <strong>Game Two</strong> to start playing</p>
+        <p>Please select <strong>Breeds List</strong> to explore all the breeds </p>
+        <p>Please select <strong>Game #1</strong> or <strong>Game #2</strong> to start playing</p>
     </div>
     </div>
     )
