93b8e0282d4d8653c6faf1a646bc2db9df9bed4f
anderara <andc91@gmail.com>
2019-11-14 13:46:06 +0100

    Article before breeds changes according to first letter of breed

--- src/components/WrongAnswerGameOne.js
+++ src/components/WrongAnswerGameOne.js
@@ -9,13 +9,20 @@ class WrongAnswerGameOne extends Component {
         let breed = this.props.answerGame.split("/")
         breed = breed[4]
 
+        let article
+        if (breed[0] === 'a' || breed[0] === 'e' || breed[0] === 'i' || breed[0] === 'o' || breed[0] === 'u'){
+            article = 'an'
+        }else{
+            article = 'a'
+        }
+
     return (
         <div>
             <br/>
             <h1> <FaRegFrown /> </h1>
             <h2> Wrong answer! </h2>
             <br/>
-            <h3>This is a {breed}!</h3>
+            <h3>This is {article} {breed}!</h3>
             <img id={this.props.answerGame} src={this.props.answerGame} width="280" height="230" alt=""/>            
         </div>
         )
