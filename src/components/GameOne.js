54400c743e1a44ec4334ee035c2906b23e45cd70
anderara <andc91@gmail.com>
2019-11-14 11:02:25 +0100

    Clean up code

--- src/components/GameOne.js
+++ src/components/GameOne.js
@@ -17,8 +17,6 @@ class GameOne extends Component {
     const Handleclick = (event) =>{
         const chosenPic = event.target.id
         const answer = chosenPic.search(correctBreed)
-        console.log('the answer is', answer)
-
         this.props.isItCorrect(answer, correctBreed)
     }
 
